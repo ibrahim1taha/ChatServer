87c6a660786d91a49bfb2da0ad365709c5cf8cb8
ibrahim Taha <1ibrahim1mohamed1@gmail.com>
2024-11-24 21:49:03 +0200

    use aggregation pipeline to get users with last msg sorted by last msg sent

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,48 +1,72 @@
-const { nextTick } = require('process');
 const userModel = require('../model/user');
 const chatModel = require('../model/chat');
 const customError = require('../utils/customError');
-// const objectId = require('mongoose').Types.ObjectId;
+const objectId = require('mongoose').Types.ObjectId;
 const socket = require('../socket');
 
 exports.getUrContact = async (req, res, next) => {
 	try {
-		const userId = req.userId;
-		const users = await userModel.find({ _id: { $ne: userId } })
-
-		if (!users) customError(404, 'no users found');
-
-		const thisUserChats = await chatModel.find({
-			$or: [
-				{ sender_id: userId },
-				{ receiver_id: userId }
-			]
-		});
-		// adding to each user last message with the sender user .
-		const usersWithLastMessage = users.map(user => {
-			for (const chat of thisUserChats) {
-				const isUserSender = (chat.receiver_id.toString() === userId && chat.sender_id.toString() === user._id.toString());
-				const isUserReceiver = (chat.sender_id.toString() === userId && chat.receiver_id.toString() === user._id.toString());
-				if (isUserSender || isUserReceiver) {
-					return {
-						...user._doc,
-						lastMsg: chat.message,
-						lastMsgTime: chat.timestamp,
-						isSender: isUserSender,
-						isReceiver: isUserReceiver
+		const OurUserId = req.userId
+		let newUsersWithLastMessage = await userModel.aggregate([
+			{
+				$match: { _id: { $ne: OurUserId } }
+			},
+			{
+				$lookup: {
+					from: "chats",
+					let: { thisUserId: "$_id" },
+					pipeline: [
+						{
+							$match: {
+								$expr: {
+									$or: [
+										{
+											$and: [
+												{ $eq: ["$sender_id", OurUserId] }, { $eq: ["$receiver_id", { $toString: "$$thisUserId" }] } // Convert ObjectId to string
+											]
+										},
+										{
+											$and: [
+												{ $eq: ["$receiver_id", OurUserId] }, { $eq: ["$sender_id", { $toString: "$$thisUserId" }] }
+											]
+										}
+									]
+								}
+							}
+						},
+						{ $sort: { timestamp: -1 } },
+						{ $limit: 1 }
+					],
+					as: "usersWithLastMsg"
+				}
+			},
+			{
+				$addFields: {
+					defaultMsg: {
+						$cond: {
+							if: { $lt: [{ $size: "$usersWithLastMsg" }, 1] },
+							then: "start conversation!",
+							else: "$$REMOVE"
+						}
 					}
 				}
+			},
+			{
+				$project: {
+					email: 0,
+					password: 0,
+					usersWithLastMsg: {
+						_id: 0
+					}
+				}
+			},
+			{
+				$sort: { "usersWithLastMsg.timestamp": -1 }
 			}
-			return {
-				...user._doc,
-				lastMsg: "start conversation",
-				isSender: false,
-				isReceiver: false
-			}
-		})
-		// console.log(usersWithLastMessage);
+		]);
+
 		res.status(200).json({
-			users: usersWithLastMessage,
+			users: newUsersWithLastMessage,
 			message: 'users fetched successfully!'
 		})
 
@@ -50,6 +74,51 @@ exports.getUrContact = async (req, res, next) => {
 		next(error)
 	}
 }
+// exports.getUrContact = async (req, res, next) => {
+// 	try {
+// 		const userId = req.userId;
+// 		const users = await userModel.find({ _id: { $ne: userId } }, { email: 0, password: 0 })
+
+// 		if (!users) customError(404, 'no users found');
+
+// 		const thisUserChats = await chatModel.find({
+// 			$or: [
+// 				{ sender_id: userId },
+// 				{ receiver_id: userId }
+// 			]
+// 		});
+// 		console.log(thisUserChats);
+// 		// adding to each user last message with the sender user .
+// 		let newUsersWithLastMessage = [];
+// 		let usersWithLastMessage = users.map(user => {
+// 			for (const chat of thisUserChats) {
+// 				const isUserSender = (chat.receiver_id.toString() === userId && chat.sender_id.toString() === user._id.toString());
+// 				const isUserReceiver = (chat.sender_id.toString() === userId && chat.receiver_id.toString() === user._id.toString());
+// 				if ((isUserSender || isUserReceiver) && chat.timestamp) {
+// 					newUsersWithLastMessage.push({
+// 						...user._doc,
+// 						lastMsg: chat.message,
+// 						lastMsgTime: chat.timestamp,
+// 						isSender: isUserSender,
+// 						isReceiver: isUserReceiver
+// 					})
+// 				}
+// 			}
+// 		})
+
+// 		newUsersWithLastMessage.sort((a, b) =>
+// 			new Date(b.lastMsgTime) - new Date(a.lastMsgTime)
+// 		)
+// 		// console.log(usersWithLastMessage);
+// 		res.status(200).json({
+// 			users: newUsersWithLastMessage,
+// 			message: 'users fetched successfully!'
+// 		})
+
+// 	} catch (error) {
+// 		next(error)
+// 	}
+// }
 
 exports.sendMessage = async (req, res, next) => {
 	try {
